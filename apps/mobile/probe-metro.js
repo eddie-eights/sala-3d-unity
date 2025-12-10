50c68a3f6412a4c38491790a4686b4886515268e
8ight <8ight@gmail.com>
2025-12-10 19:50:06 +0900

    chore: cleanup unused files and consolidate legacy configs

--- apps/mobile/probe-metro.js
+++
@@ -1,9 +0,0 @@
-try {
-  const metroConfig = require('metro-config');
-  console.log('metro-config exports:', Object.keys(metroConfig));
-} catch(e) { console.log('metro-config not found'); }
-
-try {
-  const expoMetroConfig = require('expo/metro-config');
-  console.log('expo/metro-config exports:', Object.keys(expoMetroConfig));
-} catch(e) { console.log('expo/metro-config not found'); }
